aa150b21bed7ddf440efa4a7d6bd0ddf60cb25ee
fenruko <alimzakaria61@gmail.com>
2026-09-06 13:50:28 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// EDIT THESE TWO LINES after you set up your Cloudflare Tunnel (see SETUP.md)
+const API_BASE = 'https://reduces-gibraltar-apps-regions.trycloudflare.com/api/health';
+const WS_BASE = 'wss://reduces-gibraltar-apps-regions.trycloudflare.com/ws';
