1fe12027d77738d2ca60e85dcf7927a42b00b1be
Ali <alimzakaria61@gmail.com>
2026-09-06 17:43:01 +0300

    fix

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // EDIT THESE TWO LINES after you set up your Cloudflare Tunnel (see SETUP.md)
-const API_BASE = 'https://reduces-gibraltar-apps-regions.trycloudflare.com/api';
-const WS_BASE = 'wss://reduces-gibraltar-apps-regions.trycloudflare.com/ws';
+const API_BASE = 'https://archived-mating-buddy-fallen.trycloudflare.com/api';
+const WS_BASE = 'wss://archived-mating-buddy-fallen.trycloudflare.com/ws';
