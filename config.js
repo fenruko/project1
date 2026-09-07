09678956add8847e78a4141928ceffc8bd621059
Ali <alimzakaria61@gmail.com>
2026-09-07 17:23:55 +0300

    new url

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // EDIT THESE TWO LINES after you set up your Cloudflare Tunnel (see SETUP.md)
-const API_BASE = 'https://irrigation-driving-leather-visiting.trycloudflare.com/api';
-const WS_BASE = 'wss://irrigation-driving-leather-visiting.trycloudflare.com/ws';
+const API_BASE = 'https://paying-opera-joke-annually.trycloudflare.com/api';
+const WS_BASE = 'wss://paying-opera-joke-annually.trycloudflare.com/ws';
