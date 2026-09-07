51964d391958bf9515f39cf5ae72a09e6d30b06b
Ali <alimzakaria61@gmail.com>
2026-09-07 14:39:36 +0300

    new URL

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // EDIT THESE TWO LINES after you set up your Cloudflare Tunnel (see SETUP.md)
-const API_BASE = 'https://archived-mating-buddy-fallen.trycloudflare.com/api';
-const WS_BASE = 'wss://archived-mating-buddy-fallen.trycloudflare.com/ws';
+const API_BASE = 'https://irrigation-driving-leather-visiting.trycloudflare.com/api';
+const WS_BASE = 'wss://irrigation-driving-leather-visiting.trycloudflare.com/ws';
