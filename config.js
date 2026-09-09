b72d6ed07d194736e70208a4639751655cfa99d3
Ali <alimzakaria61@gmail.com>
2026-09-09 14:00:55 +0300

    new url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // EDIT THESE TWO LINES after you set up your Cloudflare Tunnel (see SETUP.md)
-const API_BASE = 'https://porter-believed-keep-gaming.trycloudflare.com/api';
-const WS_BASE = 'wss://porter-believed-keep-gaming.trycloudflare.com/ws';
+const API_BASE = 'https://relationship-recall-creativity-advice.trycloudflare.com/api';
+const WS_BASE = 'wss://relationship-recall-creativity-advice.trycloudflare.com/ws';
 const GIPHY_API_KEY = 'KXvjmQ28CISqyPouiz1coHujH2nsZnOT';
